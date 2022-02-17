2e2934580079be3ce70053f4e174c089e2ddf311
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-02-17 19:51:15 +0600

    error handle

--- JS/money.js
+++ JS/money.js
@@ -1,35 +1,76 @@
-function updateTotalInput(inputId)
-{
-    const input = document.getElementById(inputId);
-    const inputAmountText = input.value;
-    const inputAmount = parseInt(inputAmountText);
+function updateTotalInput(inputId) {
+  const input = document.getElementById(inputId);
+  const inputAmountText = input.value;
+  const inputAmount = parseInt(inputAmountText);
 
-    input.value="";
+  const stringError = document.getElementById("string-error");
+  const negativeError = document.getElementById("negative-error");
 
+  if (isNaN(inputAmountText) || document.getElementById(inputId).value == "") {
+    stringError.style.display = "block";
+    negativeError.style.display = "none";
+
+    input.value = "";
+    return false;
+  } else if (inputAmount < 0) {
+    stringError.style.display = "none";
+    negativeError.style.display = "block";
+    input.value = "";
+
+    return false;
+  } else {
+    input.value = "";
     return inputAmount;
+  }
 }
 
-function updateTotalAmount(updateId,amount)
-{
-    const update = document.getElementById(updateId);
-    const updateText=update.innerText;
-    const updateAmount=parseInt(updateText);
-    
-    update.innerText=amount;
-}
+function updateTotalAmount(updateId, amount, isbool) {
+  const update = document.getElementById(updateId);
+  const updateText = update.innerText;
 
+  const updateAmount = parseInt(updateText);
 
-function Calculate()
-{
+  if (isbool) {
     const incomeAmount = updateTotalInput("income-Input");
+    const totalSave = parseFloat((amount * incomeAmount) / 100);
+    update.innerText = totalSave;
+  } else {
+    update.innerText = amount;
+  }
+}
 
-    const foodAmount = updateTotalInput("food-input");
-    const rentAmount=updateTotalInput("rent-input");
-    const clothesAmount = updateTotalInput("clothes-input");
+function Calculate() {
+  const incomeAmount = updateTotalInput("income-Input");
 
+  const foodAmount = updateTotalInput("food-input");
+  const rentAmount = updateTotalInput("rent-input");
+  const clothesAmount = updateTotalInput("clothes-input");
+
+  if (
+    incomeAmount == false ||
+    foodAmount == false ||
+    rentAmount == false ||
+    clothesAmount == false
+  ) {
+  } else {
     const totalExpensesAmount = foodAmount + rentAmount + clothesAmount;
-    const totalBalanceRemaining=incomeAmount-totalExpensesAmount;
+    const totalBalanceRemaining = incomeAmount - totalExpensesAmount;
 
-    updateTotalAmount("total-balance",totalBalanceRemaining);
-    updateTotalAmount("total-expenses",totalExpensesAmount);
+    updateTotalAmount("total-balance", totalBalanceRemaining, false);
+    updateTotalAmount("total-expenses", totalExpensesAmount, false);
+  }
+}
+
+function save() {
+  const saveInputAmount = updateTotalInput("save-input");
+
+  updateTotalAmount("save-amount", saveInputAmount, true);
+}
+
+function checkInp() {
+  var x = document.forms["myForm"]["age"].value;
+  if (isNaN(x)) {
+    alert("Must input numbers");
+    return false;
+  }
 }
