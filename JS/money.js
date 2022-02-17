e05c5a2a04268723e997f0842ecc68e87aa95505
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-02-17 09:23:25 +0600

    responsive

--- JS/money.js
+++ JS/money.js
@@ -1,7 +1,41 @@
+function updateTotalInput(id)
+{
+    const input = document.getElementById(id);
+    const inputAmountText = input.value;
+    const inputAmount = parseInt(inputAmountText);
+
+    input.value="";
+
+    return inputAmount;
+}
+
+function updateTotalAmount(id)
+{
+    const update = document.getAnimations("total-expenses");
+    const updateText=update.innerText;
+    const updateAmount=parent(updateText);
+}
+
+
 function Calculate()
 {
-    const incomeInput = document.getElementById("incomeInput");
-    const incomeValue=incomeInput.value();
+    const incomeAmount = updateTotalInput("income-Input");
+    const foodAmount = updateTotalInput("food-input");
+
+    // const foodAmount = updateTotalInput("food-input");
+    // const rentAmount = updateTotalInput("rent-onput");
+    // const clothesAmount = updateTotalInput("clothes-input");
 
     
+
+    // console.log(incomeAmount);
+
+    // const totalBalanceInput = document.getElementById("total-balncece");
+    // const totalBalanceText=totalBalanceInput.innerText;
+    // const totalAmount=parseInt(totalBalanceText);
+
+    const totalExpenses = updateTotalAmount("total-expenses");
+    
+
+    totalBalanceInput.innerText=incomeAmount;
 }
