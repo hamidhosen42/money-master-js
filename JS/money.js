2621c48bb2b45bf0bb7cecb76e121b7bab8bde0d
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-02-17 22:27:02 +0600

    final submit

--- JS/money.js
+++ JS/money.js
@@ -19,8 +19,8 @@ function updateTotalInput(inputId, isAdd) {
   } else if (inputAmount < 0 && isAdd == false) {
     stringError.style.display = "none";
     negativeError.style.display = "block";
-    input.value = "";
 
+    input.value = "";
     return false;
   } else {
     return inputAmount;
@@ -30,8 +30,6 @@ function updateTotalInput(inputId, isAdd) {
 // ------------all update section id------------
 function updateTotalAmount(updateId, amount) {
   const update = document.getElementById(updateId);
-  //   const updateText = update.innerText;
-  //   const updateAmount = parseInt(updateText);
 
   update.innerText = amount;
 }
@@ -44,16 +42,20 @@ function Calculate() {
   const rentAmount = updateTotalInput("rent-input", false);
   const clothesAmount = updateTotalInput("clothes-input", false);
 
+  const totalExpensesAmount = foodAmount + rentAmount + clothesAmount;
+  const totalBalanceRemaining = incomeAmount - totalExpensesAmount;
+
+  const incomeError = document.getElementById("income-error");
+
   if (
     incomeAmount == false ||
     foodAmount == false ||
     rentAmount == false ||
     clothesAmount == false
   ) {
+  } else if (incomeAmount < totalExpensesAmount) {
+    incomeError.style.display = "block";
   } else {
-    const totalExpensesAmount = foodAmount + rentAmount + clothesAmount;
-    const totalBalanceRemaining = incomeAmount - totalExpensesAmount;
-
     updateTotalAmount("total-balance", totalBalanceRemaining);
     updateTotalAmount("total-expenses", totalExpensesAmount);
   }
@@ -73,22 +75,21 @@ function save() {
   const saveInputText = saveInput.value;
   const saveAmount = parseInt(saveInputText);
 
-  console.log(saveAmount);
+  const amount = updateTotalInput("income-Input", true);
+  const totalBalance = renameBalance("total-balance");
+  const toalsaveAmount = (saveAmount * amount) / 100;
+
+  const saveError = document.getElementById("saving-error");
 
   if (
     isNaN(saveInputText) ||
     document.getElementById("save-input").value == ""
   ) {
+  } else if (toalsaveAmount > totalBalance || saveAmount > totalBalance) {
+    saveError.style.display = "block";
   } else {
-    const amount = updateTotalInput("income-Input", true);
-
-    const toalsaveAmount = (saveAmount * amount) / 100;
     document.getElementById("save-amount").innerText = toalsaveAmount;
-
-    const totalBalance = renameBalance("total-balance");
     const updatedrenameBalance = totalBalance - toalsaveAmount;
     document.getElementById("remain-balance").innerText = updatedrenameBalance;
   }
-
-  //   const saveBalance = renameBalance("save-amount");
 }
